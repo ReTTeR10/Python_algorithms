9f3d38d4f3956b5375fa507d3eafd89b46da294d
Yegor Mishin <yegor1993@gmail.com>
2018-08-28 22:23:45 +0300

    Добавлено решение

--- 1st.py
+++ 1st.py
@@ -9,3 +9,38 @@ __author__ = 'Мишин Егор Олегович'
 и снова запрашивать знак операции. 
 Также сообщать пользователю о невозможности деления на ноль, если он ввел 0 в качестве делителя.
 '''
+
+print('\n----Добро пожаловать в КАЛЬКУЛЯТОР3000!----')
+
+
+def user_input():
+
+    return input('\nЖелаемое действие? (+,-,*,/, 0 - выход) ')
+
+
+ans = user_input()
+
+while ans != '0':
+
+    num1 = float(input('\nВведите первое число'))
+    num2 = float(input('\nВведите второе число'))
+    if ans == '+':
+        print(f'Результат {num1} {ans} {num2} = {num1 + num2}')
+        ans = user_input()
+    elif ans == '-':
+        print(f'Результат {num1} {ans} {num2} = {num1 - num2}')
+        ans = user_input()
+    elif ans == '*':
+        print(f'Результат {num1} {ans} {num2} = {num1 * num2}')
+        ans = user_input()
+    elif ans == '/':
+        if num2 == 0:
+            print('Деление на 0. Попался!)')
+            ans = user_input()
+        else:
+            print(f'Результат {num1} {ans} {num2} = {num1 / num2}')
+            ans = user_input()
+    else:
+        print('Неверное действие')
+        ans = user_input()
+print('До свидания!')
