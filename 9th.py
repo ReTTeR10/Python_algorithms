b0a4358f7d8c16b41185e9779f367e43c814b5fa
Yegor Mishin <yegor1993@gmail.com>
2018-08-29 11:49:21 +0300

    добавил решение

--- 9th.py
+++ 9th.py
@@ -7,3 +7,19 @@ __author__ = 'Мишин Егор Олегович'
 '''
  спрашивает числа, пока не скажешь остнановится
 '''
+
+count = int(input('\nСколько чисел будем вводить? '))
+maximum = 0
+summa = 0
+max_num = 0
+while count != 0:
+    num = input('\nВведите число: ')
+    for i in num:
+        summa += int(i)
+    if summa > maximum:
+        maximum = summa
+        count -= 1
+        max_num = num
+    else:
+        count -= 1
+print(f'максимальное число по сумме: {max_num}, а сумма = {maximum} ')
