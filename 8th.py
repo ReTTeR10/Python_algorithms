f8dfd522cf72a39ca7d2287c17f74d35eb72f0c1
Yegor Mishin <yegor1993@gmail.com>
2018-09-01 12:00:12 +0300

    Добавил решение

--- 8th.py
+++ 8th.py
@@ -1,26 +1,22 @@
 __author__ = 'Мишин Егор Олегович'
 """
-5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
+8. Матрица 5x4 заполняется вводом с клавиатуры, кроме последних элементов строк. 
+Программа должна вычислять сумму введенных элементов каждой строки и записывать ее в ее последнюю ячейку. 
+В конце следует вывести полученную матрицу.
 """
 
-from random import randint
 
-list_1 = [randint(-9, 9) for _ in range(1, 20)]
+print('Заполнение матрицы:')
+matrix = [[int(input(f'Введите элемент\nстрока - {value}, столбец - {value1}: '))
+           for value in range(1, 5)] for value1 in range(1, 5)]
 
-max_el = -10
-max_num = []
-count = 0
-
-for value in list_1:
-
-    if value < 0 and value > max_el:
-        max_el = value
-for i, value in enumerate(list_1):      # пытался реализовать подсчет максимальных элементов в одином цикле с программой
-    if value == max_el:                 # но не получилось
-        count += 1
-        max_num.append(i)
+for row in matrix:
+    summa = 0
+    for i, value in enumerate(row):
+        summa += value
+    row.append(summa)
 
 
-print(f'Наш список: {list_1}')
-print(f'Максимальный отрицательный элемент: {max_el}, его позиция: {max_num}, встречается {count} раз')
-
+print('\nМатрица:')
+for row in matrix:
+    print(row)
