9844d28a6d9b194ddc3d52cb1c45d62add32f89b
Yegor Mishin <yegor1993@gmail.com>
2018-09-17 13:47:32 +0300

    Выполнено

--- task_3.py
+++ task_3.py
@@ -8,3 +8,31 @@ __author__ = 'Мишин Егор Олегович'
 то используйте метод сортировки, который не рассматривался на уроках.
 '''
 
+from random import randint
+
+
+def median_find(array):
+    if len(array) % 2 != 1:
+        return print('Массив должен быть с нечетным числом элементов')
+
+    for num1 in array:
+        higher = []
+        lower = []
+        median = []
+        for num2 in array:
+            if num2 < num1:
+                lower.append(num2)
+            elif num2 > num1:
+                higher.append(num2)
+            else:
+                median.append(num2)
+        if abs(len(lower) - len(higher)) == len(median) - 1:
+            return print(f'Медианой является число: {median[0]}\n'
+                         f'{lower}{median}{higher}')
+
+
+''' Генерируем рандомный список с неченым количеством элементов '''
+my_list = [randint(-100, 99) / randint(1, 10) for i in range(2 * randint(1, 10) + 1)]
+print(f' Исходный список: \n {my_list}')
+median_find(my_list)
+
