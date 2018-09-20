9f7b1a079bc46b1ea8f19dc2debafa35cd678c00
Yegor Mishin <yegor1993@gmail.com>
2018-09-20 19:29:48 +0300

    Выполнил

--- task_1.py
+++ task_1.py
@@ -5,14 +5,31 @@ __author__ = 'Мишин Егор Олегович'
 Для решения задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib или встроенную функцию hash()
 '''
 
-import hashlib
+from random import choice
+from string import ascii_lowercase
 
-my_string = 'qwertyuiopasdfghjklzxcvbnm'
 
-# ht = "ghghgh";
-t = my_string
-m = 9
-for i in range(m):
-    my_string += pow[i] * t[i]
+my_string = ''   # формируем рандомную строку
+num = int(input('Введите длину строки: '))
+my_string += ''.join(choice(ascii_lowercase) for i in range(num))
+print(f'Ваша строка: \n {my_string}')
 
-print(my_string)
+our_hash = []
+
+for i in range(len(my_string) + 1):
+    for j in range(i + 1, len(my_string) + 1):
+        if (not hash(my_string[i:j]) in our_hash) and (not my_string[i:j] == my_string):
+            our_hash.append(hash(my_string[i:j]))
+
+print(f'{len(our_hash)} подстрок(а) в исходной строке: \n {my_string}')
+
+'''
+Был такой пример при num = 232
+'''
+#
+# Ваша строка:
+#  hfbsxyuhlkgeodaqlwvregzousuufgwjzwtglngjrqqdjxxzzgbhnakwatnstwgblhsawzcqumobnchggkxesuqjlepsdlxapkcxbqiztfcntanhrqwrgoolnjfgkqdetkcrowrefvefrsvpqkvcbaufydbzayhuttgjxmigvlwxzaemlmdaiwibsvieczkiawiwresoomlfjpucvwcnopvfouqlchfqectljooq
+#
+# 26783 подстрок в исходной строке:
+#  hfbsxyuhlkgeodaqlwvregzousuufgwjzwtglngjrqqdjxxzzgbhnakwatnstwgblhsawzcqumobnchggkxesuqjlepsdlxapkcxbqiztfcntanhrqwrgoolnjfgkqdetkcrowrefvefrsvpqkvcbaufydbzayhuttgjxmigvlwxzaemlmdaiwibsvieczkiawiwresoomlfjpucvwcnopvfouqlchfqectljooq |
+
